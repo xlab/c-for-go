aefca10dde88086626327978c6a07d5b05affa92
Maxim Kupriianov <max@kc.vc>
2015-07-29 03:29:38 +0300

    Some tests for translator.

---
+++ src/github.com/xlab/cgogen/translator/test/translator_test.h
@@ -0,0 +1,10 @@
+typedef const unsigned char * int8_t;
+typedef int8_t alias_t;
+
+typedef struct test {
+	char A;
+	char B;
+} test_t;
+
+int8_t plus (int8_t a, int8_t b);
+int8_t minus (int8_t c, int8_t d);
