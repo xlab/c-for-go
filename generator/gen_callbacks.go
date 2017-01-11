cd6e36b444f3e518c765e0620e83086c89acd30d
Maxim Kupriianov <max@kc.vc>
2017-01-11 05:00:36 +0300

    Fix.

--- generator/gen_callbacks.go
+++ generator/gen_callbacks.go
@@ -240,7 +240,7 @@ func (gen *Generator) proxyCallbackArgToGo(memTip tl.Tip, varName, ptrName strin
 	if goSpec.IsGoString() {
 		helper := gen.getPackStringHelper(cgoSpec)
 		gen.submitHelper(helper)
-		proxy = fmt.Sprintf("%s = %s(%s)", varName, helper.Name, ptrName)
+		proxy = fmt.Sprintf("%s := %s(%s)", varName, helper.Name, ptrName)
 		return proxy, helper.Nillable
 	} else if getHelper, ok := toGoHelperMap[goSpec]; ok {
 		helper := getHelper(gen, cgoSpec)
